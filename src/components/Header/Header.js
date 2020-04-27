183755e95aa07677adfc4de3d69e51363258ea4a
Christopher Goulet <chris@uproot.tech>
2020-04-26 22:39:17 -0400

    rpm logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-import logo from  '../../React-icon.svg';
+import logo from  '../../rpm.png';
 
 
 library.add(faBars)
